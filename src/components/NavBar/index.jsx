264c894b177f6157e2e0ca65aab4d32cabb5c9e0
Matias Molesini <Mhmolesini@gmail.com>
2023-05-02 20:49:45 -0300

    Estilos al header

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { CartWidget } from "../CartWidget";
+import logoZahria from '../../assets/images/logos/1.webp'
+import './Navbar.css'
 
 export const NavBar = () => {
     return(
         <>
-            <nav>
-                <ul>
+            <nav className='header'>
+                <div className="header__logo"><img src={logoZahria} alt="Logo de Zahria" /></div>
+                <ul className="header__navbar">
                     <li>Pantalones</li>
                     <li>Remeras</li>
-                    <li><CartWidget/>1</li>
+                    <li>Camperas</li>
                 </ul>
+                <div className="header__cart"><CartWidget/>1</div>
             </nav>
         </>
     )
